278b664621c897e5e210d5ea777618523fc52ef0
Timmy <timmy85618@gmail.com>
2025-08-14 16:00:52 +0800

    prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,18 @@
-import js from '@eslint/js';
-import tseslint from '@typescript-eslint/eslint-plugin';
-import tsparser from '@typescript-eslint/parser';
-import reactHooks from 'eslint-plugin-react-hooks';
-import globals from 'globals';
+import js from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+import reactHooks from "eslint-plugin-react-hooks";
+import globals from "globals";
 
 export default [
   js.configs.recommended,
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       parser: tsparser,
       parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
+        ecmaVersion: "latest",
+        sourceType: "module",
         project: true,
         tsconfigRootDir: import.meta.dirname,
       },
@@ -20,27 +20,27 @@ export default [
         ...globals.browser,
         ...globals.es2020,
         ...globals.node,
-        APIResponse: 'readonly',
-        APIError: 'readonly',
-        React: 'readonly',
-        NodeJS: 'readonly',
+        APIResponse: "readonly",
+        APIError: "readonly",
+        React: "readonly",
+        NodeJS: "readonly",
       },
     },
     plugins: {
-      '@typescript-eslint': tseslint,
-      'react-hooks': reactHooks,
+      "@typescript-eslint": tseslint,
+      "react-hooks": reactHooks,
     },
     rules: {
       ...tseslint.configs.recommended.rules,
-      ...tseslint.configs['recommended-requiring-type-checking'].rules,
+      ...tseslint.configs["recommended-requiring-type-checking"].rules,
       ...reactHooks.configs.recommended.rules,
-      'react-hooks/exhaustive-deps': 'off',
-      '@typescript-eslint/no-misused-promises': 'off',
-      '@typescript-eslint/no-non-null-assertion': 'off',
-      '@typescript-eslint/no-empty-function': 'off',
-      '@typescript-eslint/no-floating-promises': 'off',
-      '@typescript-eslint/no-unused-expressions': 'off',
-      '@typescript-eslint/no-empty-object-type': 'off',
+      "react-hooks/exhaustive-deps": "off",
+      "@typescript-eslint/no-misused-promises": "off",
+      "@typescript-eslint/no-non-null-assertion": "off",
+      "@typescript-eslint/no-empty-function": "off",
+      "@typescript-eslint/no-floating-promises": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
     },
   },
 ];
